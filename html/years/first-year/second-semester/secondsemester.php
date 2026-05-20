645a1ddb7fed02a721e04b354722f89db2aabc0d
Alowna <155198875+Alowna@users.noreply.github.com>
2026-05-20 20:48:07 +0000

    php that shit up

--- html/years/first-year/second-semester/secondsemester.php
+++ html/years/first-year/second-semester/secondsemester.php
@@ -18,19 +18,19 @@
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                     <li>
-                        <a href="../semesters.html" class="nav-link px-2 text-white">1º Ano</a>
+                        <a href="/years/first-year/semesters.php" class="nav-link px-2 text-white">1º Ano</a>
                     </li>
                     <li>
-                        <a href="#" class="nav-link px-2 text-white">Don't know yet!</a>
+                        <a href="/wip.php" class="nav-link px-2 text-white">Don't know yet!</a>
                     </li>
                     <li>
-                        <a href="#" class="nav-link px-2 text-white">Don't know yet!</a>
+                        <a href="/wip.php" class="nav-link px-2 text-white">Don't know yet!</a>
                     </li>
                     <li>
-                        <a href="#" class="nav-link px-2 text-white">Don't know yet!</a>
+                        <a href="/wip.php" class="nav-link px-2 text-white">Don't know yet!</a>
                     </li>
                     <li>
-                        <a href="#" class="nav-link px-2 text-white">Don't know yet!</a>
+                        <a href="/wip.php" class="nav-link px-2 text-white">Don't know yet!</a>
                     </li>
 
                 </ul>
--- html/years/first-year/semesters.php
+++ html/years/first-year/semesters.php
@@ -3,30 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GeoCheatSheet Homepage</title>
-    <script type="module" src="/components/headImports.js"></script>
-    <script type="module" src="/components/WIP.js"></script>
+    <title>Primeiro Ano GeoCheatSheet</title>
 
+    
+    <script type="module" src="/components/headImports.js"></script>
     <head-imports></head-imports>
 
 </head>
-<body  class="d-flex flex-column min-vh-100">
+<body class="d-flex flex-column min-vh-100">
     <header class="p-3 bg-dark text-white">
         <div class="container d-flex flex-wrap justify-content-center">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                     <li>
-                        <a href="index.html" class="nav-link px-2 text-white">Voltar</a>
+                        <a href="/" class="nav-link px-2 text-white">Página Inicial</a>
+                    </li>
+                    <li>
+                        <a href="/years/first-year/first-semester/firstsemester.php" class="nav-link px-2 text-white">1º Semestre</a>
+                    </li>
+                    <li>
+                        <a href="/wip.php" class="nav-link px-2 text-white">2º Semestre</a>
                     </li>
                 </ul>
             </div>
         </div>
     </header>
 
-    <!-- <main class="flex-grow-1"></main> -->
 
-    <wip-component></wip-component>
-    
+    <main class="flex-grow-1">
+    <h1>POR ENQUANTO NADA AQUI!</h1>
+    </main>
 
     <footer-component></footer-component>
 </body>
