84a15517acc08f7a8bdc12a7770f152905eb95dc
Alowna <155198875+Alowna@users.noreply.github.com>
2026-05-22 17:52:08 +0000

    what the dog doing

--- html/components/headImports.php
+++ html/components/headImports.php
@@ -12,6 +12,7 @@ $stylesSrc = cacheReset('/styles/style.css');
         connectedCallback() {
 
             document.head.insertAdjacentHTML("beforeend", `
+                <link rel="icon" type="image/png" href="/img/whathedogdoing.png">
                 <link rel="preconnect" href="https://fonts.googleapis.com">
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
                 <link href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap" rel="stylesheet">
